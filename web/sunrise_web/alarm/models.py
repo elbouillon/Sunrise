28ef88927a6406a7dc45c35633e6bf0746926ca8
wkurmann <willy.kurmann@gmail.com>
2017-07-16 01:00:27 +0200

    Added weekday fields

--- web/sunrise_web/alarm/models.py
+++ web/sunrise_web/alarm/models.py
@@ -1,6 +1,13 @@
-from mongoengine import Document, IntField,  BooleanField
+from mongoengine import Document, IntField, BooleanField, ListField
 
 class Alarm(Document):
     time = IntField(min_value=0, max_value=2359, verbose_name='Time')
     active = BooleanField(default=False)
+    weekday_1 = BooleanField(default=False, verbose_name='MO')
+    weekday_2 = BooleanField(default=False, verbose_name='TU')
+    weekday_3 = BooleanField(default=False, verbose_name='WE')
+    weekday_4 = BooleanField(default=False, verbose_name='TH')
+    weekday_5 = BooleanField(default=False, verbose_name='FR')
+    weekday_6 = BooleanField(default=False, verbose_name='SA')
+    weekday_7 = BooleanField(default=False, verbose_name='SU')
 
