8ac2df83cefab451fade578626a3ad4b78fedfeb
wkurmann <willy.kurmann@gmail.com>
2017-07-15 02:21:34 +0200

    Added verbose_name to fields to define a custom name to display

--- web/sunrise_web/alarm/models.py
+++ web/sunrise_web/alarm/models.py
@@ -1,6 +1,6 @@
 from mongoengine import Document, IntField,  BooleanField
 
 class Alarm(Document):
-    time = IntField(min_value=0, max_value=2359)
+    time = IntField(min_value=0, max_value=2359, verbose_name='Time')
     active = BooleanField(default=False)
 
