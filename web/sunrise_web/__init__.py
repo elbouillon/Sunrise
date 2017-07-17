11965a4822c3b28d60323a452dcb2a1c4ebf76ee
wkurmann <willy.kurmann@gmail.com>
2017-07-17 21:17:31 +0200

    Removed bootstrap and import controllers

--- web/sunrise_web/__init__.py
+++ web/sunrise_web/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_basicauth import BasicAuth
-from flask_bootstrap import Bootstrap
 from flask_mongoengine import MongoEngine
 
 from sunrise_web import config
-from sunrise_web.alarm import alarm_blueprint
+from sunrise_web.alarm.controllers import alarm_blueprint
 
 # MongoEngine
 db = MongoEngine()
@@ -22,9 +21,6 @@ def create_app(**config_overrides):
 
     # Basic auth
     BasicAuth(app)
-
-    # Bootstrap
-    Bootstrap(app)
 
      # Setup the database.
     db.init_app(app)
