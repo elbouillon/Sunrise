f3da87e1b362623f790be47206caa8c534fe251e
wkurmann <willy.kurmann@gmail.com>
2017-07-20 21:38:04 +0200

    Added gulp task for autocompiling scss

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
-browserSync = require('browser-sync'),
-process = require('child_process');
+    browserSync = require('browser-sync'),
+    process = require('child_process'),
+    sass = require('gulp-sass');
 
 var reload = browserSync.reload;
 var exec = process.exec;
@@ -9,12 +10,18 @@ gulp.task('runserver', function() {
     var proc = exec('python manage.py runserver');
 });
 
-gulp.task('default', ['runserver'], function () {
-  browserSync({
-    notify: false,
-    proxy: "127.0.0.1:5000/alarm"
-  });
- 
-  gulp.watch(['sunrise_web/alarm/templates/**'], reload);
+gulp.task('styles', function() {
+    gulp.src('sunrise_web/static/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('sunrise_web/static/css/'));
+});
 
+gulp.task('default', ['runserver'], function() {
+    browserSync({
+        notify: false,
+        proxy: "127.0.0.1:5000/alarm"
+    });
+
+    gulp.watch(['sunrise_web/alarm/templates/**'], reload);
+    gulp.watch('sunrise_web/static/sass/*.scss', ['styles']);
 });
