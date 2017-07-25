0ce0b9f42e1fdf1979f71e0c87f121cc341c76d9
wkurmann <willy.kurmann@gmail.com>
2017-07-25 17:37:28 +0200

    Updated CheckAlarmWorker

--- runtime/sunrise_runtime/workers.py
+++ runtime/sunrise_runtime/workers.py
@@ -1,36 +1,55 @@
 import threading
 import time
 import re
+import dateutil.parser
 from datetime import datetime
 
-from web import JsonClient
+
+from web import AlarmClient
 
 class CheckAlarmWorker(threading.Thread):
     def __init__(self, config):
         threading.Thread.__init__(self)
-        self.__client = JsonClient(config.SUNRISE_WEB)
+        self.__alarm_client = AlarmClient(config.SUNRISE_WEB)
 
     def run(self):
-        alarm_match = False
+        match = False
 
-        while not alarm_match:
-            alarm = self.__client.get_alarm()
+        while not match:
+            time.sleep(1)
+            try:
+                alarm = self.__alarm_client.get()
+                match = self.__check_alarm(alarm)
+            except:
+                print 'Unexpected error:', sys.exc_info()[0]
 
-            if alarm is not None and alarm['active'] == True:
-                alarm_regex = self.__get_alarm_regex(alarm)
-                current_time_str = self.__get_current_time_str()
+        try:
+            now = datetime.now().replace(second=0, microsecond=0)  
+            self.__alarm_client.update_last_run_on(now)
+        except:
+            print('Could not update alarm last_run_on time')
+            raise
 
-                if re.match(alarm_regex, current_time_str):
-                    alarm_match = True
+    def __check_alarm(self, alarm):
+        if alarm is None or alarm['active'] == False:
+            return False
 
-            if not alarm_match:            
-                time.sleep(3)
-    
+        if 'last_run_on' in alarm:
+            last_run_on = dateutil.parser.parse(alarm['last_run_on'])
+            timedelta = datetime.now() - last_run_on
+            if timedelta.total_seconds() < 60:
+                return False
+
+        alarm_regex = self.__get_alarm_regex(alarm)
+        current_time_str = self.__get_current_time_str()
+
+        return re.match(alarm_regex, current_time_str)
+
     def __get_alarm_regex(self, alarm):
         hour = alarm['hour']
         minute = alarm['minute']
-
         days_of_week = []
+
         if alarm['day_of_week_1']: days_of_week.append('0')
         if alarm['day_of_week_2']: days_of_week.append('1')
         if alarm['day_of_week_3']: days_of_week.append('2')
